9153eb937395064a7dda437c69679e48c10236d4
Ricardo Pontaza <pontaza.ricardo@gmail.com>
2025-07-27 20:27:08 +0400

    Added script to reproduce results presented in Figure 3 in the manuscript.

---
+++ reproduce_results/manuscript_figure_03/benchmark_graph_03.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+barWidth = 0.25
+fig = plt.subplots(figsize =(12, 8))
+
+pattern = re.compile("(32m)([0-9]+)")
+
+# for i, line in enumerate(open('../c-code/cmake-build-release-cycles-x8664/test_bench_01.txt')):
+#     for match in re.finditer(pattern, line):
+#         print(f'Found on line %s: %s' % (i+1, match.group(2)))
+#
+# for i, line in enumerate(open('../c-code/cmake-build-release-cycles-x8664/test_bench_02.txt')):
+#     for match in re.finditer(pattern, line):
+#         print(f'Found on line %s: %s' % (i+1, match.group(2)))
+#
+# for i, line in enumerate(open('../c-code/cmake-build-release-cycles-x8664/test_bench_04.txt')):
+#     for match in re.finditer(pattern, line):
+#         print(f'Found on line %s: %s' % (i+1, match.group(2)))
+
+average_cglhash2 = []
+average_cglhash3 = []
+
+# 1. Check for p254
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p254-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+# 2. Check for p252
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p255-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+# 3. Check for p381
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p381-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+# 4. Check for p383
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p383-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+# 5. Check for p398
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p398-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+# 6. Check for p511
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p511-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+# 7. Check for p575
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p575-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+# 8. Check for p592
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p592-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+# 9. Check for p765
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p765-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+# 10. Check for p783
+matching_values = []
+for i, line in enumerate(open('../../c-code/cmake-build-release-cycles-x8664-graph/benchmarks_ssec-p783-output.txt')):
+    for match in re.finditer(pattern, line):
+        matching_values.append(int(match.group(2)))
+average_cglhash2.append(matching_values[0])
+average_cglhash3.append(matching_values[1])
+
+
+IT = average_cglhash2
+ECE = average_cglhash3
+
+br1 = np.arange(len(IT))
+br2 = [x + barWidth for x in br1]
+
+plt.bar(br1, IT, color ='r', width = barWidth, edgecolor ='grey', label ='IT')
+plt.bar(br2, ECE, color ='b', width = barWidth, edgecolor ='grey', label ='ECE')
+
+plt.xlabel('Primes', fontweight ='bold', fontsize = 15)
+plt.ylabel('CPU Cycles', fontweight ='bold', fontsize = 15)
+plt.xticks([r + barWidth for r in range(len(IT))],
+           ['254', '255', '381'])
+
+plt.title('Benchmarks for the 2-isogenies vs. 3-isogenies walks')
+
+plt.legend()
+plt.show()
---
+++ reproduce_results/manuscript_figure_03/generate_figure_3.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd ../../c-code
+echo "cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARKING=CYCLES -DARCHITECTURE=x8664 -B cmake-build-release-cycles-x8664-graph"
+cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARKING=CYCLES -DARCHITECTURE=x8664 -B cmake-build-release-cycles-x8664-graph
+echo "cd cmake-build-release-cycles-x8664-graph"
+cd cmake-build-release-cycles-x8664-graph
+echo "make -j"
+make -j
+
+echo "benchmarks/benchmarks-ssec-p254 | tee benchmarks_ssec-p254-output.txt"
+benchmarks/benchmarks-ssec-p254 | tee benchmarks_ssec-p254-output.txt
+
+echo "benchmarks/benchmarks-ssec-p255 | tee benchmarks_ssec-p255-output.txt"
+benchmarks/benchmarks-ssec-p255 | tee benchmarks_ssec-p255-output.txt
+
+echo "benchmarks/benchmarks-ssec-p381 | tee benchmarks_ssec-p381-output.txt"
+benchmarks/benchmarks-ssec-p381 | tee benchmarks_ssec-p381-output.txt
+
+echo "benchmarks/benchmarks-ssec-p383 | tee benchmarks_ssec-p383-output.txt"
+benchmarks/benchmarks-ssec-p383 | tee benchmarks_ssec-p383-output.txt
+
+echo "benchmarks/benchmarks-ssec-p398 | tee benchmarks_ssec-p398-output.txt"
+benchmarks/benchmarks-ssec-p398 | tee benchmarks_ssec-p398-output.txt
+
+echo "benchmarks/benchmarks-ssec-p511 | tee benchmarks_ssec-p511-output.txt"
+benchmarks/benchmarks-ssec-p511 | tee benchmarks_ssec-p511-output.txt
+
+echo "benchmarks/benchmarks-ssec-p575 | tee benchmarks_ssec-p575-output.txt"
+benchmarks/benchmarks-ssec-p575 | tee benchmarks_ssec-p575-output.txt
+
+echo "benchmarks/benchmarks-ssec-p592 | tee benchmarks_ssec-p592-output.txt"
+benchmarks/benchmarks-ssec-p592 | tee benchmarks_ssec-p592-output.txt
+
+echo "benchmarks/benchmarks-ssec-p765 | tee benchmarks_ssec-p765-output.txt"
+benchmarks/benchmarks-ssec-p765 | tee benchmarks_ssec-p765-output.txt
+
+echo "benchmarks/benchmarks-ssec-p783 | tee benchmarks_ssec-p783-output.txt"
+benchmarks/benchmarks-ssec-p783 | tee benchmarks_ssec-p783-output.txt
+
+cd ../../reproduce_results/manuscript_figure_03
+python3 benchmark_graph_03.py
